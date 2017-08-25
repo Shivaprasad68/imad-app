e842fd5c8d61729ce096e07778c99e5db3422fd0
Shivaprasad68 <31250080+Shivaprasad68@users.noreply.github.com>
2017-08-25 20:17:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function(req, res)
 {
-    res.send('this is serverone belongs to shiva prasad');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req, res)
