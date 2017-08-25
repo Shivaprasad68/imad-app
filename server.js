6b1650001b7d6f2105137176eec8d950e0557281
Shivaprasad68 <31250080+Shivaprasad68@users.noreply.github.com>
2017-08-25 20:32:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/article-two', function(req, res)
 
 app.get('/article-three', function(req, res)
 {
-    res.send('this is serverthree belongs to chintu');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
