8158a9c8ff9035b694169dccb12f35ac068e27a7
Shivaprasad68 <31250080+Shivaprasad68@users.noreply.github.com>
2017-08-26 15:04:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft= marginLeft + 10;
+    marginLeft= marginLeft + 5;
     image.style.marginLeft= marginLeft + 'px';
 }
 img.oneclick = function(){
-    var interval = setInterval(moveRight,0);
+    var interval = setInterval(moveRight,50);
 };
