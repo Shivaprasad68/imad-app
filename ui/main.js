db5ce7968610a82aba1853780701996f1d13bc8a
Shivaprasad68 <31250080+Shivaprasad68@users.noreply.github.com>
2017-08-26 15:00:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,13 @@ console.log('Loaded!');
 var element= document.getElementById('maintext');
 element.innerHTML='my name is shivaprasad dude';
 
-var image=document.getElementById('madi');
-image.oneclick = function(){
-    image.style.marginLeft='100px';
+var img=document.getElementById('madi');
+var marginLeft=0;
+function marginRight()
+{
+    marginLeft= marginLeft + 10;
+    image.style.marginLeft= marginLeft + 'px';
+}
+img.oneclick = function(){
+    var interval = setInterval(moveRight,0);
 };
