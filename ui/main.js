bd2e6dde1ec82a6e2a657e34f55e5131008fcce3
Shivaprasad68 <31250080+Shivaprasad68@users.noreply.github.com>
2017-08-26 17:49:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,9 @@
-console.log('Loaded!');
+//counter code
+var button= document.getElementById('counter');
+var counter =0;
 
-var element= document.getElementById('maintext');
-element.innerHTML='my name is shivaprasad dude';
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft= marginLeft + 5;
-    image.style.marginLeft= marginLeft + 'px';
-}
-img.oneclick = function(){
-    var interval = setInterval(moveRight,50);
+button.onclick = function(){
+    counter = counter+1;
+    var span= document.getElemenById('count');
+    span.innerHTML= counter.toString();
 };
