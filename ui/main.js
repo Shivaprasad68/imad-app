20be73ab72ed450e3cc705c90fd9db14010b723c
Shivaprasad68 <31250080+Shivaprasad68@users.noreply.github.com>
2017-08-26 14:43:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element= document.getElementById('maintext');
 element.innerHTML='my name is shivaprasad dude';
+
+var image=document.getElementById('madi');
+image.oneclick = function(){
+    image.style.marginleft='200px';
+};
