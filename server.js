f2914e5e82d290d299e80d14a37eeda5031e7b41
Shivaprasad68 <31250080+Shivaprasad68@users.noreply.github.com>
2017-08-26 16:56:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,6 +66,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function (req,res){
+    counter=counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/article-three', function(req, res)
 {
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
